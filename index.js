ecbbc8715aec0fec29852eb5198a7a5639629f67
Hugo M Aseka <82669254+HugoAseka@users.noreply.github.com>
2022-06-15 20:56:38 -0300

    Corrigindo data de corpus christi

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const holidays = [
   { date: "4/17/2022", name: "Páscoa" },
   { date: "4/21/2022", name: "Tiradentes" },
   { date: "5/1/2022", name: "Dia do trabalho" },
-  { date: "6/15/2022", name: "Corpus Christi" },
+  { date: "6/16/2022", name: "Corpus Christi" },
   { date: "9/7/2022", name: "Independência do Brasil" },
   { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
   { date: "11/2/2022", name: "Finados" },
